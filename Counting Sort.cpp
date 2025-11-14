eee94ce2f33dfba8cb7ead4c19ce600b859ba279
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2025-11-15 00:03:54 +0600

    Counting Sort.cpp

--- Counting Sort.cpp
+++ Counting Sort.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 const int MAX = 100;
 int32_t main() {
+  
   ios_base::sync_with_stdio(0);
   cin.tie(0);
+  
   int n; cin >> n;
   vector<int> cnt(MAX + 1, 0);
   for (int i = 1; i <= n; i++) {
